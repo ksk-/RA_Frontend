657917c993d34fdd8c53d0910ccd6c1ac76838e7
ksk- <iksk810@yandex.ru>
2020-06-06 19:06:08 +0700

    Small fix of imports.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import '@fancyapps/fancybox/dist/jquery.fancybox.css';
 import '@babel/polyfill';
 import 'isomorphic-fetch';
 
-import 'normalize.css/normalize.css';
-
 import './js/main';
 import './scss/style.scss';
